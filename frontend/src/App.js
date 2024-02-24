5d0a8c164e714738422866c0a37c2cd487420387
hitakritgoplani <hitakritgoplani@gmail.com>
2024-02-24 17:28:06 +0530

    frontend contents

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Home from './pages/Home';
+import Add from './pages/Add';
+import Multiply from './pages/Multiply';
+import Compare from './pages/Comparison';
+import Count from './pages/Counting';
+import Settings from './pages/Settings';
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/home" element={<Home/>} />
+          <Route path="/addition" element={<Add/>} />
+          <Route path="/multiply" element={<Multiply/>} /> 
+          <Route path='/counting' element={<Count />} /> 
+          <Route path='/comparison' element={<Compare />} /> 
+          <Route path='/settings' element={<Settings />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ frontend/src/pages/Counting.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Counting() {
+  return (
+    <div>
+      Count
+    </div>
+  )
+}
