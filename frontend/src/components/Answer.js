ec6f5cfa5263f652ed38d3dc511ad6c8a186c20d
hitakritgoplani <hitakritgoplani@gmail.com>
2024-02-27 19:41:19 +0530

    random number and enter

--- frontend/src/components/Answer.js
+++ frontend/src/components/Answer.js
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react'
 import '../styles/Answer.css'
 
-export default function Answer() {
+export default function Answer(props) {
+    const [inputValue, setInputValue] = useState('');
+
+    const handleKeyPress = (event) => {
+        if (event.key === 'Enter') {
+            console.log(inputValue)
+            // Process the answer here, for now, let's just clear the input field
+            props.onEnterPressed();
+            setInputValue('');
+        }
+    };
     return (
         <div className='answer-root'>
-            <input className="answer-input" placeholder="Enter Answer" type="text" />
+            <input value={inputValue} onChange={(e) => setInputValue(e.target.value)} onKeyPress={handleKeyPress} className="answer-input" placeholder="Enter Answer" type="text" />
         </div>
 
     )
--- frontend/src/pages/Add.js
+++ frontend/src/pages/Add.js
@@ -1,25 +1,37 @@
-import React from 'react'
-import Header from '../components/Header'
-import '../styles/Add.css'
-import Question from '../components/Question'
-import Line from '../components/Line'
-import Answer from '../components/Answer'
+import React, { useState } from 'react';
+import Header from '../components/Header';
+import '../styles/Add.css';
+import Question from '../components/Question';
+import Line from '../components/Line';
+import Answer from '../components/Answer';
 
 export default function Add() {
+    const [question1, setQuestion1] = useState(generateRandomNumber());
+    const [question2, setQuestion2] = useState(generateRandomNumber());
+
+    function generateRandomNumber() {
+        return Math.floor(Math.random() * 1001) + 1;
+    }
+
+    function handleEnterPressed() {
+        setQuestion1(generateRandomNumber());
+        setQuestion2(generateRandomNumber());
+    }
+
     return (
         <div>
             <Header />
             <div className='add-root'>
                 <div className='add-question'>
-                    <Question number="5"/>
+                    <Question number={question1}/>
                     <div style={{fontSize:"15rem"}}><strong>+</strong></div>
-                    <Question number="100"/>
+                    <Question number={question2}/>
                 </div>
                 <Line />
                 <div className='add-answer'>
-                    <Answer  styles={{width:"10vw"}}/>
+                    <Answer styles={{width:"10vw"}} onEnterPressed={handleEnterPressed}/>
                 </div>
             </div>
         </div>
-    )
+    );
 }
